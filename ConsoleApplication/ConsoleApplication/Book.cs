17d360e8e6e617865ccb80b89df454f311555e29
Jonppa <joni.karhunen@hotmail.com>
2019-01-22 12:07:13 +0200

    Assingment: Book Class

---
+++ ConsoleApplication/ConsoleApplication/Book.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Book
+    {
+
+        public string bookAuthor;
+        public string bookTitle;
+        public string bookId;
+        public float bookPrice;
+
+
+        public Book(string bookAuthor, string bookTitle, string bookId, float bookPrice)
+        {
+            this.bookAuthor = bookAuthor;
+            this.bookTitle = bookTitle;
+            this.bookId = bookId;
+            this.bookPrice = bookPrice;
+        }
+
+        public string Kuvaus()
+        {
+            return ID + " " + Author + " " + Title + " " + string.Format("{0:0.00}",Price);
+        }
+
+        public string Title
+        {
+            get { return bookTitle; }
+            set { bookTitle = value; }
+        }
+
+        public string Author
+        {
+            get { return bookAuthor; }
+            set { bookAuthor = value; }
+        }
+
+        public float Price
+        {
+            get { return bookPrice; }
+            set { bookPrice = value; }
+        }
+
+        public string ID
+        {
+            get { return bookId; }
+            set { bookId = value; }
+        }
+
+
+        public string Vertailu(Book book)
+        {
+            if (this.bookPrice > book.bookPrice)
+                return $"{this.bookTitle} on kalliimpi kuin {book.bookTitle}";
+
+            if (this.bookPrice < book.bookPrice)
+                return $"{this.bookTitle} on halvempi kuin {book.bookTitle}";
+
+            if (this.bookPrice == book.bookPrice)
+                return $"{this.bookTitle} on halvempi kuin {book.bookTitle}";
+            return "";
+
+        }
+    }
+}
---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string author;
+            string title;
+            int price;
+
+
+
+            Console.WriteLine("Press enter to add new book...");
+            Console.ReadLine();
+
+            Console.WriteLine("Title of the book:");
+            title = Console.ReadLine();
+            Console.WriteLine("Name of the author:");
+            author = Console.ReadLine();
+            Console.WriteLine("Price for the book:");
+            price = int.Parse(Console.ReadLine());
+
+            Console.WriteLine()*/
+
+            
+
+            Book book1 = new Book("Olli Jalonen", "Taivaanpallo", "00001", 24.95F);
+            Console.WriteLine(book1.Kuvaus());
+
+            Book book2 = new Book("Blake Crouch", "Pimeää ainetta", "00002", 29.99F);
+            Console.WriteLine(book2.Kuvaus());
+
+            Book book3 = new Book("Paula Hawkins", "Tummiin vesiin", "00003", 19.99F);
+            Console.WriteLine(book3.Kuvaus());
+
+            Console.WriteLine(book1.Vertailu(book2));
+
+            Console.ReadLine();
+        }
+    }
+}
